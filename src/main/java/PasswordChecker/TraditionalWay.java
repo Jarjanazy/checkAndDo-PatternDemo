d4013dc6e0ad8e9237d0e4fb750a1ffbd144c374
jarjanazy <celil.cercenazi@gmail.com>
2020-12-30 16:27:09 +0300

    Add the initial Password Checker Traditional Way.

---
+++ src/main/java/PasswordChecker/TraditionalWay.java
@@ -0,0 +1,9 @@
+package PasswordChecker;
+
+// length should be between 8 and 14
+// should have at least one upper case and one lower case at least
+// should have at least 2 digits
+// should have at least one special character
+public class TraditionalWay {
+
+}
---
+++ src/test/java/PasswordCheckerTest/TraditionalWayTest.java
@@ -0,0 +1,14 @@
+package PasswordCheckerTest;
+
+import PasswordChecker.TraditionalWay;
+import org.junit.jupiter.api.Test;
+
+public class TraditionalWayTest {
+    TraditionalWay traditionalWay = new TraditionalWay();
+
+    @Test
+    void givenAnAlbum_DetectItsNotNWA(){
+
+    }
+}
+
