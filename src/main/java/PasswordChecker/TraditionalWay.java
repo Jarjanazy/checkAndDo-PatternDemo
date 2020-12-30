b9ff7daf296a03d4183034e59cb17754e170e3f5
jarjanazy <celil.cercenazi@gmail.com>
2020-12-30 19:18:43 +0300

    Add a number of checks to the Password Checker Traditional Way.

--- src/main/java/PasswordChecker/TraditionalWay.java
+++ src/main/java/PasswordChecker/TraditionalWay.java
@@ -5,5 +5,17 @@ package PasswordChecker;
 // should have at least 2 digits
 // should have at least one special character
 public class TraditionalWay {
+    public String checkPassword(String password) {
+        int length = password.length();
 
+        if (length < 8) return "Password is under 8 characters";
+
+        if (length > 14) return "Password is above 14 characters";
+
+        if (password.equals(password.toLowerCase())) return "Password must have at least one uppercase letter";
+
+        if (password.equals(password.toUpperCase())) return "Password must have at least one lowercase letter";
+
+        else return "Success";
+    }
 }
--- src/test/java/PasswordCheckerTest/TraditionalWayTest.java
+++ src/test/java/PasswordCheckerTest/TraditionalWayTest.java
@@ -3,12 +3,44 @@ package PasswordCheckerTest;
 import PasswordChecker.TraditionalWay;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TraditionalWayTest {
     TraditionalWay traditionalWay = new TraditionalWay();
 
     @Test
-    void givenAnAlbum_DetectItsNotNWA(){
+    void givenAString_DetectItsLengthIsUnder8(){
+        String password = "myPass";
+        String result = traditionalWay.checkPassword(password);
+        assertEquals("Password is under 8 characters", result);
+    }
 
+    @Test
+    void givenAString_DetectItsOver14(){
+        String password = "myPasswordIsVeryLong";
+        String result = traditionalWay.checkPassword(password);
+        assertEquals("Password is above 14 characters", result);
+    }
+
+    @Test
+    void givenAString_DetectNoUpperCaseIsGiven(){
+        String password = "passnocapital";
+        String result = traditionalWay.checkPassword(password);
+        assertEquals("Password must have at least one uppercase letter", result);
+    }
+
+    @Test
+    void givenAString_DetectNoLowerCaseIsGiven(){
+        String password = "PASSNOLOWER";
+        String result = traditionalWay.checkPassword(password);
+        assertEquals("Password must have at least one lowercase letter", result);
+    }
+
+    @Test
+    void givenAString_AssertSuccess(){
+        String password = "validPassword";
+        String result = traditionalWay.checkPassword(password);
+        assertEquals("Success", result);
     }
 }
 
