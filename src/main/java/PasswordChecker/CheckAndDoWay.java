6ff170792ac7299e633e091621dd40034c1a10bf
jarjanazy <celil.cercenazi@gmail.com>
2021-01-01 19:08:24 +0300

    Add the full CheckAndDoWay.

--- src/main/java/PasswordChecker/CheckAndDoWay.java
+++ src/main/java/PasswordChecker/CheckAndDoWay.java
@@ -1,5 +1,8 @@
 package PasswordChecker;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CheckAndDoWay {
 
     public String checkPasswordLengthAndSubmit(String password) {
@@ -8,7 +11,33 @@ public class CheckAndDoWay {
         if (length < 8) return "Password is under 8 characters";
 
         if (length > 14) return "Password is above 14 characters";
+        
+        return checkPassWordCaseSensitivityAndSubmit(password);
+    }
 
-        else return "Submitted";
+    private String checkPassWordCaseSensitivityAndSubmit(String password) {
+        if (password.equals(password.toLowerCase())) return "Password must have at least one uppercase letter";
+
+        if (password.equals(password.toUpperCase())) return "Password must have at least one lowercase letter";
+
+        return checkThatTwoDigitsAreGivenAndSubmit(password);
+    }
+
+    private String checkThatTwoDigitsAreGivenAndSubmit(String password) {
+        if(password.replaceAll("\\D", "").length() < 2) return "Password must have at least two digits";
+
+        return checkThatSpecialCharactersAreGivenAndSubmit(password);
+    }
+
+    private String checkThatSpecialCharactersAreGivenAndSubmit(String password) {
+        Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(password);
+        if (!m.find()) return "Password must have at least one special character";
+
+        return submit(password);
+    }
+
+    private String submit(String password) {
+        return "Submitted";
     }
 }
--- src/test/java/PasswordCheckerTest/PasswordCheckerTest.java
+++ src/test/java/PasswordCheckerTest/PasswordCheckerTest.java
@@ -33,35 +33,45 @@ public class PasswordCheckerTest {
     void givenAString_DetectNoUpperCaseIsGiven(){
         String password = "passnocapital";
         String result = traditionalWay.checkPasswordAndSubmit(password);
+        String result2 = checkAndDoWay.checkPasswordLengthAndSubmit(password);
         assertEquals("Password must have at least one uppercase letter", result);
+        assertEquals("Password must have at least one uppercase letter", result2);
     }
 
     @Test
     void givenAString_DetectNoLowerCaseIsGiven(){
         String password = "PASSNOLOWER";
         String result = traditionalWay.checkPasswordAndSubmit(password);
+        String result2 = checkAndDoWay.checkPasswordLengthAndSubmit(password);
         assertEquals("Password must have at least one lowercase letter", result);
+        assertEquals("Password must have at least one lowercase letter", result2);
     }
 
     @Test
     void givenAString_DetectNoTwDigitsIsGiven(){
         String password = "PassWord1";
         String result = traditionalWay.checkPasswordAndSubmit(password);
+        String result2 = checkAndDoWay.checkPasswordLengthAndSubmit(password);
         assertEquals("Password must have at least two digits", result);
+        assertEquals("Password must have at least two digits", result2);
     }
 
     @Test
     void givenAString_DetectNoSpecialCharacterIsGiven(){
         String password = "PassWord01";
         String result = traditionalWay.checkPasswordAndSubmit(password);
+        String result2 = checkAndDoWay.checkPasswordLengthAndSubmit(password);
         assertEquals("Password must have at least one special character", result);
+        assertEquals("Password must have at least one special character", result2);
     }
 
     @Test
     void givenAString_AssertSuccess(){
         String password = "validPass12.";
         String result = traditionalWay.checkPasswordAndSubmit(password);
+        String result2 = checkAndDoWay.checkPasswordLengthAndSubmit(password);
         assertEquals("Submitted", result);
+        assertEquals("Submitted", result2);
     }
 }
 
